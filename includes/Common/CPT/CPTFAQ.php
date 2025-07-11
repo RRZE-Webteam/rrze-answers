0ec72e826d2d671fdbb6f2f762b617909d4f1e3b
Benjamin Klemencic <benjamin.klemencic@fau.de>
2025-07-11 16:52:19 +0200

    Update CPTFAQ.php

--- includes/Common/CPT/CPTFAQ.php
+++ includes/Common/CPT/CPTFAQ.php
@@ -3,7 +3,10 @@
 namespace RRZE\Answers\Common\CPT;
 
 defined('ABSPATH') || exit;
+<<<<<<< Updated upstream:includes/Common/CPT/CPTFAQ.php
 use RRZE\Answers\Common\Config;
+=======
+>>>>>>> Stashed changes:includes/Common/CPT/FAQ.php
 
 class CPTFAQ
 {
