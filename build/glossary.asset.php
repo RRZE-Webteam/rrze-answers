d8a5b3460c286d5f81776f29d03d02d6fc43adde
Benjamin Klemencic <benjamin.klemencic@fau.de>
2026-02-25 14:28:08 +0100

    1.0.24

--- build/glossary.asset.php
+++ build/glossary.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9149e4a42607681f3641');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '949d1a5143b004e4ec69');
