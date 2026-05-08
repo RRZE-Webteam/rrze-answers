9cbf0401018c16d9689cac7b24a3b33394566b3d
Benjamin Klemencic <benjamin.klemencic@fau.de>
2026-05-08 09:46:12 +0200

    1.3.5 (improves layout placeholder dropdown)

--- build/placeholder.asset.php
+++ build/placeholder.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '4e4b492e04af949a69e2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-rich-text', 'wp-server-side-render'), 'version' => '13e86aab9e07c2cfc87a');
